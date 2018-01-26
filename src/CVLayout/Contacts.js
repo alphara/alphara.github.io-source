eb0c02d468ae6eaed4a061524859521c949f275d
alpharak <artarakcheev@gmail.com>
2018-01-26 08:16:20 -0200

    Add contact buttons

--- src/CVLayout/Contacts.js
+++ src/CVLayout/Contacts.js
@@ -3,7 +3,10 @@ import { List, Icon, Button } from 'semantic-ui-react'
 
 export default function Contacts(props) {
   return (
-    <List>
+    <List horizontal>
+      <List.Item>
+      </List.Item>
+
       <List.Item>
         <Button color='teal' as='a' target='_blank'
           href='//alphara.github.io'
@@ -46,6 +49,96 @@ export default function Contacts(props) {
         >
           <Icon name='facebook' />
           Facebook
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='blue' as='a' target='_blank'
+          href='https://www.kaggle.com/arakcheev'
+        >
+          <Icon name='kaggle' />
+          Kaggle
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='orange' as='a' target='_blank'
+          href='https://stackoverflow.com/users/3069279/alphara'
+        >
+          <Icon name='stackoverflow' />
+          Stack Overflow
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='grey' as='a' target='_blank'
+          href='https://angel.co/artem-arakcheev'
+        >
+          <Icon name='angellist' />
+          AngelList
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='red' as='a' target='_blank'
+          href='https://www.meetup.com/arakcheev/'
+        >
+          <Icon name='meetup' />
+          Meetup
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='youtube' as='a' target='_blank'
+          href='https://www.youtube.com/c/ArtemArakcheev'
+        >
+          <Icon name='youtube' />
+          YouTube
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='google plus' as='a' target='_blank'
+          href='https://plus.google.com/+ArtemArakcheev'
+        >
+          <Icon name='google plus' />
+          Google+
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='pinterest' as='a' target='_blank'
+          href='https://pinterest.com/artarakcheev/'
+        >
+          <Icon name='pinterest' />
+          Pinterest
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='instagram' as='a' target='_blank'
+          href='https://www.instagram.com/artarakcheev/'
+        >
+          <Icon name='instagram' />
+          Instagram
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='orange' as='a' target='_blank'
+          href='https://soundcloud.com/alphara-1'
+        >
+          <Icon name='soundcloud' />
+          SoundCloud
+        </Button>
+      </List.Item>
+
+      <List.Item>
+        <Button color='lignt blue' as='a' target='_blank'
+          href='skype:hahumor'
+        >
+          <Icon name='skype' />
+          Skype
         </Button>
       </List.Item>
 
